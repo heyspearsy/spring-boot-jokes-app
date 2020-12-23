758f2879b824e991fc086876045d02d77934d844
Steven Spears <sspears@deloitte.ca>
2020-12-23 14:18:47 -0500

    add Banner

--- src/main/java/ca/vitalbydesign/jokesapp/controller/JokeController.java
+++ src/main/java/ca/vitalbydesign/jokesapp/controller/JokeController.java
@@ -1,6 +1,7 @@
 package ca.vitalbydesign.jokesapp.controller;
 
 import ca.vitalbydesign.jokesapp.service.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ public class JokeController {
 
     JokeService jokeService;
 
+    @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
